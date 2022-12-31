f18e9ad86030c299c230ca3c8054d8a286918754
lizongti <lzongti@gmail.com>
2022-12-31 16:33:33 +0800

    fix brew && pyenv

--- pyenv.sh
+++ pyenv.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-brew install pyenv gcc
+brew install pyenv pyenv-virtualenv gcc
 echo "alias pyenv='CC=\"$(brew --prefix gcc)/bin/gcc-12\" pyenv'" >> ~/.bash_aliases
+echo "export PYENV_ROOT=\"\$HOME/.pyenv\"" >> ~/.bash_profile
+echo "export PATH=\"\$PYENV_ROOT/bin:\$PATH\"" >> ~/.bash_profile
+echo "eval \"\$(pyenv init -)\"" >> ~/.bash_profile
+echo "eval \"\$(pyenv virtualenv-init -)\"" >> ~/.bash_profile
