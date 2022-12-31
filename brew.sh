2d086c83a91c31639e6a6a5732996103597520ba
lizongti <lzongti@gmail.com>
2023-01-01 00:39:12 +0800

    update installer

--- brew.sh
+++ brew.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" || true
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv) && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >> ~/.bashrc
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" || true && \
+export PATH=/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH && \
+brew shellenv && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >> ~/.bashrc && \
---
+++ gcc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+brew install gcc && \
+mv -f /home/linuxbrew/.linuxbrew/bin/x86_64-pc-linux-gnu-gfortran-12 /home/linuxbrew/.linuxbrew/bin/x86_64-pc-linux-gnu-gfortran && \
+mv -f /home/linuxbrew/.linuxbrew/bin/x86_64-pc-linux-gnu-gcc-ranlib-12 /home/linuxbrew/.linuxbrew/bin/x86_64-pc-linux-gnu-gcc-ranlib && \
+mv -f /home/linuxbrew/.linuxbrew/bin/x86_64-pc-linux-gnu-gcc-nm-12 /home/linuxbrew/.linuxbrew/bin/x86_64-pc-linux-gnu-gcc-nm && \
+mv -f /home/linuxbrew/.linuxbrew/bin/x86_64-pc-linux-gnu-gcc-ar-12 /home/linuxbrew/.linuxbrew/bin/x86_64-pc-linux-gnu-gcc-ar && \
+mv -f /home/linuxbrew/.linuxbrew/bin/x86_64-pc-linux-gnu-gcc-12 /home/linuxbrew/.linuxbrew/bin/x86_64-pc-linux-gnu-gcc && \
+mv -f /home/linuxbrew/.linuxbrew/bin/x86_64-pc-linux-gnu-g++-12 /home/linuxbrew/.linuxbrew/bin/x86_64-pc-linux-gnu-g++ && \
+mv -f /home/linuxbrew/.linuxbrew/bin/x86_64-pc-linux-gnu-c++-12 /home/linuxbrew/.linuxbrew/bin/x86_64-pc-linux-gnu-c++ && \
+mv -f /home/linuxbrew/.linuxbrew/bin/lto-dump-12 /home/linuxbrew/.linuxbrew/bin/lto-dump && \
+mv -f /home/linuxbrew/.linuxbrew/bin/gfortran-12 /home/linuxbrew/.linuxbrew/bin/gfortran && \
+mv -f /home/linuxbrew/.linuxbrew/bin/gcov-tool-12 /home/linuxbrew/.linuxbrew/bin/gcov-tool && \
+mv -f /home/linuxbrew/.linuxbrew/bin/gcov-dump-12 /home/linuxbrew/.linuxbrew/bin/gcov-dump && \
+mv -f /home/linuxbrew/.linuxbrew/bin/gcov-12 /home/linuxbrew/.linuxbrew/bin/gcov && \
+mv -f /home/linuxbrew/.linuxbrew/bin/gcc-ranlib-12 /home/linuxbrew/.linuxbrew/bin/gcc-ranlib && \
+mv -f /home/linuxbrew/.linuxbrew/bin/gcc-nm-12 /home/linuxbrew/.linuxbrew/bin/gcc-nm && \
+mv -f /home/linuxbrew/.linuxbrew/bin/gcc-ar-12 /home/linuxbrew/.linuxbrew/bin/gcc-ar && \
+mv -f /home/linuxbrew/.linuxbrew/bin/gcc-12 /home/linuxbrew/.linuxbrew/bin/gcc && \
+mv -f /home/linuxbrew/.linuxbrew/bin/g++-12 /home/linuxbrew/.linuxbrew/bin/g++ && \
+mv -f /home/linuxbrew/.linuxbrew/bin/cpp-12 /home/linuxbrew/.linuxbrew/bin/cpp && \
+mv -f /home/linuxbrew/.linuxbrew/bin/c++-12 /home/linuxbrew/.linuxbrew/bin/c++
