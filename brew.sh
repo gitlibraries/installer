cb3ac32200801b9a5f4d7c58ccd40283c2f04933
lizongti <lzongti@gmail.com>
2022-12-31 19:44:00 +0800

    fix installer

--- brew.sh
+++ brew.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" || true
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv) && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >> ~/.bash_profile
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv) && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >> ~/.bashrc
--- pyenv.sh
+++ pyenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 brew install pyenv pyenv-virtualenv gcc
 echo "alias pyenv='CC=\"$(brew --prefix gcc)/bin/gcc-12\" pyenv'" >> ~/.bash_aliases
-echo "export PYENV_ROOT=\"\$HOME/.pyenv\"" >> ~/.bash_profile
-echo "export PATH=\"\$PYENV_ROOT/bin:\$PATH\"" >> ~/.bash_profile
-echo "eval \"\$(pyenv init -)\"" >> ~/.bash_profile
-echo "eval \"\$(pyenv virtualenv-init -)\"" >> ~/.bash_profile
+echo "export PYENV_ROOT=\"\$HOME/.pyenv\"" >> ~/.bashrc
+echo "export PATH=\"\$PYENV_ROOT/bin:\$PATH\"" >> ~/.bashrc
+echo "eval \"\$(pyenv init -)\"" >> ~/.bashrc
+echo "eval \"\$(pyenv virtualenv-init -)\"" >> ~/.bashrc
