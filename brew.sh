8b84e7b2c29c9c461e9c961efedff8a5e5d28bbc
lizongti <lzongti@gmail.com>
2022-12-31 18:40:47 +0800

    fix brew.sh

--- brew.sh
+++ brew.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" || true
 test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv) || true
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv) || true
 test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile || true
